3892b314234e18ff4e5b344c9fbe648dc0a980ec
rohitrai <raistudying@gmail.com>
2023-01-02 23:02:41 +0530

    index.js: comment requirement

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -8,7 +8,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <h1>This is the Photo Share project</h1>
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
     </div>
   );
 }
