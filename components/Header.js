f70d61658042b484d9682d67e7f8fede202f722a
rohitrai <raistudying@gmail.com>
2023-01-14 10:45:26 +0530

    Header.js: style search

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* Logo */}
-        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
             src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/photo-share-text.png"
             fill
@@ -14,7 +14,7 @@ function Header() {
           />
         </div>
 
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
             fill
@@ -23,12 +23,17 @@ function Header() {
         </div>
 
         {/* Middle */}
-        <div>
-          <div>
-            <MagnifyingGlassIcon />
+        <div className="relative mt-1 p-3 rounded-md">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+            <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
           </div>
-          <input type="text" placeholder="Search" />
+          <input
+            className="bg-gray-50 block w-full pl-10 sm:text-sm broder-gray-300 focus:ring-black focus:border-black rounded-md"
+            type="text"
+            placeholder="Search"
+          />
         </div>
+        
         {/* RIght */}
       </div>
     </div>
