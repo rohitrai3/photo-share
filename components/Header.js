5b5b6c4992623f56aff2cff02b43d73b9f4e79c7
rohitrai <raistudying@gmail.com>
2023-01-14 10:17:37 +0530

    Header.js: style photoshare text logo

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,14 @@ import Image from 'next/image';
 function Header() {
   return (
     <div>
-      {/* Left */}
-      <div>
-        <Image src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/photo-share-text.png" width="100" height="100" />
+      <div className="flex justify-between max-w-6xl">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid h-24 w-24">
+          <Image
+            src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/photo-share-text.png"
+            fill style={{ objectFit: "contain" }}
+          />
+        </div>
       </div>
       {/* Middle */}
 
