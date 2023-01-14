d1e9d1e4aa1e6d176e7cc5d5a5c987f84075bbac
rohitrai <raistudying@gmail.com>
2023-01-14 10:10:44 +0530

    Header.js: photoshare text logo

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
+import Image from 'next/image';
+
 function Header() {
   return (
     <div>
       {/* Left */}
-
+      <div>
+        <Image src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/photo-share-text.png" width="100" height="100" />
+      </div>
       {/* Middle */}
 
       {/* RIght */}
