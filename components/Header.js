178d13823c4fd551578b141dfce2d5ec63e31c1e
rohitrai <raistudying@gmail.com>
2023-01-14 10:20:34 +0530

    Header.js: logo

--- components/Header.js
+++ components/Header.js
@@ -8,13 +8,22 @@ function Header() {
         <div className="relative hidden lg:inline-grid h-24 w-24">
           <Image
             src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/photo-share-text.png"
-            fill style={{ objectFit: "contain" }}
+            fill
+            style={{ objectFit: "contain" }}
           />
         </div>
-      </div>
-      {/* Middle */}
 
-      {/* RIght */}
+        <div>
+          <Image
+            src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        {/* Middle */}
+
+        {/* RIght */}
+      </div>
     </div>
   );
 }
