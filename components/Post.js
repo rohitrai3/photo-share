ba6625b1cb14f84d4c95f0b4357a7cebfa586738
rohitrai <raistudying@gmail.com>
2023-01-15 16:29:49 +0530

    Post.js: username

--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,4 @@
-function Post({ userImg }) {
+function Post({ username, userImg }) {
   return (
     <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
@@ -8,6 +8,7 @@ function Post({ userImg }) {
           className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
           alt="User Profile"
         />
+        <p className="flex-1 font-bold">{username}</p>
       </div>
 
       {/* Image */}
