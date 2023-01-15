e6930c38ac877b6f15b130ddd2bf538d0fcdc1f0
rohitrai <raistudying@gmail.com>
2023-01-15 19:08:27 +0530

    MiniProfile.js: style miniprofile

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,6 +1,6 @@
 function MiniProfile() {
   return (
-    <div>
+    <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="w-16 h-16 rounded-full border p-[2px]"
         src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/logo.png"
