b1be1beca05659dddf03a693b189af381712eabc
rohitrai <raistudying@gmail.com>
2023-01-17 09:17:16 +0530

    Suggestions.js: style suggestions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -16,28 +16,31 @@ function Suggestions() {
   }, []);
 
   return (
-    <div>
+    <div className="mt-4 ml-10">
       <div className="flex justify-between text-sm mb-5">
         <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
         <button className="text-gray-600 font-semibold">See All</button>
       </div>
 
       {suggestions.map((profile) => (
-        <div>
+        <div
+          key={profile.id}
+          className="flex items-center justify-between mt-3"
+        >
           <img
             className="w-10 h-10 rounded-full border p-[2px]"
             src={profile.avatar}
             alt="User Profile"
           />
 
-          <div>
+          <div className="flex-1 ml-4">
             <h2 className="font-semibold text-sm">{profile.username}</h2>
             <h3 className="text-xs text-gray-400">Works at {profile.companyName}</h3>
           </div>
 
-          <button>Follow</button>
+          <button className="text-blue-400 text-xs font-bold">Follow</button>
         </div>
-      ))};
+      ))}
     </div>
   );
 };
