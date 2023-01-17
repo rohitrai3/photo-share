37e68d5c18d38ab9d2d7c729c76e64bf98bf86b0
rohitrai <raistudying@gmail.com>
2023-01-17 08:36:11 +0530

    Suggestions.js: set fake suggestions data

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,4 +1,20 @@
+import { faker } from '@faker-js/faker';
+import { useState, useEffect } from 'react';
+
 function Suggestions() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      id: i,
+      avatar: faker.internet.avatar(),
+      username: faker.internet.userName(),
+      companyName: faker.company.companyName(),
+    }));
+
+    setSuggestions(suggestions);
+  }, []);
+
   return (
     <div>
       <h1>I am suggestions.</h1>
