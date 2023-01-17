04f2141aef83804836b4e85cebf289fa56a255ad
rohitrai <raistudying@gmail.com>
2023-01-17 09:00:34 +0530

    Suggestions.js: style suggestions header

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -17,8 +17,10 @@ function Suggestions() {
 
   return (
     <div>
-      <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
-      <button className="text-gray-600 font-semibold">See All</button>
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">See All</button>
+      </div>
     </div>
   );
 };
