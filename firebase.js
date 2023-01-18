bde28863f2ac07f52a71f9c92b9afe98c49685a1
rohitrai <raistudying@gmail.com>
2023-01-18 08:12:57 +0530

    firebae.js: initial config

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIqexXqfcPMhbBp1cukRH2e2luXwu41G8",
+  authDomain: "photo-share-91677.firebaseapp.com",
+  projectId: "photo-share-91677",
+  storageBucket: "photo-share-91677.appspot.com",
+  messagingSenderId: "67396078484",
+  appId: "1:67396078484:web:db7829762e97033f199d81",
+  measurementId: "G-74PVENKJ0H",
+};
+
+const app = initializeApp(firebaseConfig);
