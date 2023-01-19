87091b926ca278046834bd228af6d7a585edf7f0
rohitrai <raistudying@gmail.com>
2023-01-19 22:19:25 +0530

    install headlessui

--- components/Modal.js
+++ components/Modal.js
@@ -1,7 +1,11 @@
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
+
 function Modal() {
+  const [open, setOpen] = useRecoilState(modalState);
   return (
     <div>
-      <h1>i am a modal.</h1>
+      {open && <h1>i am a modal.</h1>}
     </div>
   );
 };
