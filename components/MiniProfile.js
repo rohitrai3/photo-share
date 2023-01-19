dc7fa05fab3642fa6cbf294889a1aeab3f541b64
rohitrai <raistudying@gmail.com>
2023-01-19 20:44:07 +0530

    MiniProfile.js: Sign Out functionality

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,8 +1,8 @@
-import { useSession } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 
 function MiniProfile() {
   const { data: session } = useSession();
-  
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
@@ -16,7 +16,7 @@ function MiniProfile() {
         <h3 className="text-sm text-gray-400">Welcome to PhotoShare</h3>
       </div>
 
-      <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
+      <button onClick={signOut} className="text-blue-400 text-sm font-semibold">Sign Out</button>
     </div>
   );
 };
