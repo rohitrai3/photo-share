d29ca5889347b3fe25490c74414347629318462a
rohitrai <raistudying@gmail.com>
2023-01-19 21:24:55 +0530

    Stories.js: show user story at first

--- components/Stories.js
+++ components/Stories.js
@@ -1,8 +1,10 @@
 import { faker } from '@faker-js/faker';
 import { useEffect, useState } from 'react';
 import Story from './Story';
+import { useSession } from 'next-auth/react';
 
 function Stories() {
+  const { data: session } = useSession();
   const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
@@ -11,12 +13,19 @@ function Stories() {
       userName: faker.internet.userName(),
       avatar: faker.internet.avatar(),
     }));
-    
+
     setSuggestions(suggestions);
   }, []);
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story
+          img={session.user.image}
+          username={session.user.username}
+        />
+      )}
+
       {suggestions.map((profile) => (
         <Story
           key={profile.id}
