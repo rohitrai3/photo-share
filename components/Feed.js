658b7226713ed8e863c4a2ae8ea8a7151fcd4c21
rohitrai <raistudying@gmail.com>
2023-01-19 21:10:41 +0530

    Feed.js: center feed if not logged in

--- components/Feed.js
+++ components/Feed.js
@@ -6,9 +6,9 @@ import { useSession } from 'next-auth/react';
 
 function Feed() {
   const { data: session } = useSession();
-  
+
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+    <main className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
       <section className="col-span-2">
         {/* Stories */}
         <Stories />
