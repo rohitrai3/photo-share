eaf1fec000516429f14b8102dbb2a7ffc7b4aa81
rohitrai <raistudying@gmail.com>
2023-01-19 09:11:37 +0530

    signin.js: style signin button

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,7 +6,7 @@ function SignIn({ providers }) {
     <>
       <Header />
 
-      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-10 px-14 text-center">
         <img
           className="w-80"
           src="https://raw.githubusercontent.com/rohitrai3/resources/main/images/photo-share-text.png"
@@ -15,13 +15,21 @@ function SignIn({ providers }) {
         <p className="font-xs italic">
           Upload your photos to share them with Everyone
         </p>
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
+
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() => 
+                  signIn(provider.id, { callbackUrl: "/" })
+                }
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
