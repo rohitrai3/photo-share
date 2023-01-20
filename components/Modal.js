3e051a7a6267650e8e559a8ca14d829bfb0770dd
rohitrai <raistudying@gmail.com>
2023-01-20 08:04:30 +0530

    Modal.js: camera icon

--- components/Modal.js
+++ components/Modal.js
@@ -2,6 +2,7 @@ import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment } from 'react';
+import { CameraIcon } from '@heroicons/react/24/outline';
 
 function Modal() {
   const [open, setOpen] = useRecoilState(modalState);
@@ -45,6 +46,14 @@ function Modal() {
           >
             <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
               <div>
+                <div
+                  className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                >
+                  <CameraIcon
+                    className="h-6 w-6 text-red-600"
+                    aria-hidden="true"
+                  />
+                </div>
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
