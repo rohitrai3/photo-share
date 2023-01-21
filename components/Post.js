176c17edd8f40cb3d1b88628491b024fb9132a36
rohitrai <raistudying@gmail.com>
2023-01-21 11:48:28 +0530

    Post.js: comment's time:

--- components/Post.js
+++ components/Post.js
@@ -10,6 +10,7 @@ import { addDoc, collection, onSnapshot, orderBy, serverTimestamp, query } from 
 import { db } from '../firebase';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
+import Moment from 'react-moment';
 
 function Post({ id, username, userImg, img, caption }) {
   const { data: session } = useSession();
@@ -95,6 +96,10 @@ function Post({ id, username, userImg, img, caption }) {
                 <span className="font-bold">{comment.data().username}</span>{" "}
                 {comment.data().comment}
               </p>
+
+              <Moment fromNow className="pr-5 text-xs">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
             </div>
           ))}
         </div>
